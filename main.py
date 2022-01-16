34f43f895107630db03f9e07986382b08c55d207
davetpj <dhkim92@icloud.com>
2022-01-16 21:12:08 +0900

    kofic,naver movie data crawler

---
+++ main.py
@@ -0,0 +1,8 @@
+from worker import kofic_upcoming, get_naver_url, get_naver_review, merge_data
+
+
+if __name__ == "__main__":
+    kofic_upcoming
+    get_naver_url
+    get_naver_review
+    merge_data
